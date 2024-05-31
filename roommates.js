7051645d76e961572dabdafbfd9a8bac1a75ce36
Romina <rominchuela@gmail.com>
2024-05-31 03:14:59 -0400

    Reparo algunos errores

--- roommates.js
+++ roommates.js
@@ -7,6 +7,61 @@ const fs = require('fs');
 //uuid genera un id unico de 36 caracteres
 const { v4: uuidv4 } = require('uuid');
 
+//////////////////////////////////////////////////////////////////
+//calcular debe recibe total
+
+let monto;
+let cuota;
+// let finanza;
+// let recibeMonto;
+// let debeMonto;
+
+async function calculo() {
+    const { gastos } = JSON.parse(fs.readFileSync("./data/gastos.json", "utf8"));
+    let roommatesData = JSON.parse(fs.readFileSync("./data/roommates.json", "utf8"));
+    // console.log('monto', gastosData.gastos[0].monto) //el unico gasto que hay
+    let totalMontos = gastos.reduce((total, gasto) => total + gasto.monto, 0);
+    //para identificar comprador
+    // monto = totalMontos;
+    cuota = totalMontos / roommatesData.roommates.length;
+
+    roommatesData.roommates.forEach(r => {
+        let recibeMonto = 0;
+        let debeMonto = 0;
+        gastos.forEach(gasto => {
+            if (gasto.roommate === r.nombre) {
+                console.log('cuot',cuota)
+                console.log(roommatesData.roommates.length)
+                recibeMonto = cuota * (roommatesData.roommates.length - 1);
+                // debeMonto = cuota;
+            } else {
+                debeMonto = cuota;
+            }
+        });
+
+        r.debe = Math.floor(debeMonto);
+        r.recibe = Math.floor(recibeMonto);
+        r.total = recibeMonto - debeMonto;
+
+        console.log(`Finanza para ${r.nombre}:`, { debe: r.debe, recibe: r.recibe, total: r.total });
+    });
+    // return roommatesData.roommates;
+    fs.writeFileSync("./data/roommates.json", JSON.stringify(roommatesData, null, 2));
+
+}
+
+// let finanza;
+// calculo().then(result => {
+//     finanza = result;
+//     console.log('Roommates data outside the function:', finanza[0]);
+//     // Ahora puedes usar la variable `roommates` como desees
+// });
+// console.log(roommates)
+// console.log('total montos', monto)
+// console.log('pa cada uno', recibeMonto, debeMonto);
+
+//////////////////////////////////////////////////////////////////
+
 async function crearRoomie(){
 
         // Almacenar roommates nuevos ocupando random user. 
@@ -23,6 +78,8 @@ async function crearRoomie(){
             total: 0
         };
 
+        calculo();
+        
         console.log('Roommate creado:', roommate);
 
         // Leer el archivo roomates.json
